a23b539d74f4d392648f7ed0ac240d2e11735a89
teleivo <teleivo@users.noreply.github.com>
2021-11-05 06:07:35 +0100

    chore: test run()

--- main.go
+++ main.go
@@ -24,7 +24,12 @@ func main() {
 }
 
 func run(args []string, out io.Writer) error {
-	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
+	// ExitOnError makes the error message look cleaner to the user
+	// but makes testing hard. ContinueOnError allows me to capture the
+	// returned error. Unfortunately, flag will print the error and usage and
+	// main() will print the error again.
+	// TODO is there a way to handle this better?
+	flags := flag.NewFlagSet(args[0], flag.ContinueOnError)
 	src := flags.String("src", "", "Source directory containing Maven Surefire XML reports.")
 	dest := flags.String("dest", "", "Destination directory where CSV(s) will be written to. It will be created if does not exist.")
 	concat := flags.Bool("concat", false, "Concatenate all Maven Surefire XML reports into one CSV file.")
--- main_test.go
+++ main_test.go
@@ -4,11 +4,63 @@ import (
 	"bytes"
 	"os"
 	"path"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
+
+func TestRun(t *testing.T) {
+	tc := map[string]struct {
+		args []string
+		err  string
+	}{
+		"UnkownFlagFailsParsing": {
+			args: []string{
+				"sure",
+				"-unkownflag",
+			},
+			err: "-unkownflag",
+		},
+		"SrcIsMandatory": {
+			args: []string{
+				"sure",
+				"-dest",
+				t.TempDir(),
+			},
+			err: "src must be provided",
+		},
+		"DestIsMandatory": {
+			args: []string{
+				"sure",
+				"-src",
+				t.TempDir(),
+			},
+			err: "dest must be provided",
+		},
+	}
+
+	for k, tc := range tc {
+		t.Run(k, func(t *testing.T) {
+			var out bytes.Buffer
+
+			err := run(tc.args, &out)
+
+			if tc.err != "" {
+				if err == nil {
+					t.Fatal("expected an error but got none")
+				}
+				if !strings.Contains(err.Error(), tc.err) {
+					t.Fatalf("got error %q but want %q", err, tc.err)
+				}
+			}
+			if tc.err == "" && err != nil {
+				t.Fatalf("expected no error but got: %s", err)
+			}
+		})
+	}
+}
 
 func TestCsvConverter(t *testing.T) {
 	tc := map[string]struct {
