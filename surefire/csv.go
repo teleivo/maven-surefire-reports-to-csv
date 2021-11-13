b4f0b5fe550be1be8fa117e383c04ed2e1d8f549
teleivo <teleivo@users.noreply.github.com>
2021-11-13 10:37:11 +0100

    chore: move helpers down
    so exported types are at the top

--- surefire/csv.go
+++ surefire/csv.go
@@ -14,56 +14,6 @@ import (
 	"sync"
 )
 
-func header() []string {
-	return []string{
-		"module",
-		"class",
-		"test",
-		"test duration [seconds]",
-		"test suite duration [seconds]",
-		"test suite tests [number]",
-		"test suite errors [number]",
-		"test suite skipped [number]",
-		"test suite failures [number]",
-		"basedir",
-	}
-}
-
-func records(r io.Reader) ([][]string, error) {
-	var suite TestSuite
-	err := xml.NewDecoder(r).Decode(&suite)
-	if err != nil {
-		return nil, err
-	}
-
-	var basedir string
-	var module string
-	for _, v := range suite.Properties.Properties {
-		if v.Name == "basedir" {
-			basedir = v.Value
-			module = filepath.Base(v.Value)
-		}
-	}
-
-	var records [][]string
-	for _, c := range suite.Cases {
-		records = append(records, []string{
-			module,
-			c.ClassName,
-			c.Name,
-			c.Time,
-			suite.Time,
-			suite.Tests,
-			suite.Errors,
-			suite.Skipped,
-			suite.Failures,
-			basedir,
-		})
-	}
-
-	return records, nil
-}
-
 type CsvConverter struct {
 	From   string
 	Concat bool
@@ -231,4 +181,54 @@ func (sc *separateConverter) Close() error {
 func CSVFilename(file string) string {
 	fn := filepath.Base(file)
 	return strings.TrimSuffix(fn, filepath.Ext(fn)) + ".csv"
+}
+
+func header() []string {
+	return []string{
+		"module",
+		"class",
+		"test",
+		"test duration [seconds]",
+		"test suite duration [seconds]",
+		"test suite tests [number]",
+		"test suite errors [number]",
+		"test suite skipped [number]",
+		"test suite failures [number]",
+		"basedir",
+	}
+}
+
+func records(r io.Reader) ([][]string, error) {
+	var suite TestSuite
+	err := xml.NewDecoder(r).Decode(&suite)
+	if err != nil {
+		return nil, err
+	}
+
+	var basedir string
+	var module string
+	for _, v := range suite.Properties.Properties {
+		if v.Name == "basedir" {
+			basedir = v.Value
+			module = filepath.Base(v.Value)
+		}
+	}
+
+	var records [][]string
+	for _, c := range suite.Cases {
+		records = append(records, []string{
+			module,
+			c.ClassName,
+			c.Name,
+			c.Time,
+			suite.Time,
+			suite.Tests,
+			suite.Errors,
+			suite.Skipped,
+			suite.Failures,
+			basedir,
+		})
+	}
+
+	return records, nil
 }
